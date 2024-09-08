df192b75bbd5c66133e5fb342026427bee11eef7
LawInBee <18606145168@163.com>
2024-09-08 23:09:23 +0800

    pr test

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 // TODO: rewrite index.css when control the entire theme.
 import "./index.css";
-//  我是测试
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
